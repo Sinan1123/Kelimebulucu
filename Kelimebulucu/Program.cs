6f79aac07f426ab0e149b5d805b30e48cf26c2d1
sinan12344 <150492820+sinan12344@users.noreply.github.com>
2025-05-04 16:41:18 +0300

    Add project files.

---
+++ Kelimebulucu/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Kelimebulucu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kelimeler = { "elma", "armut", "elma", "muz", "elma", "armut" };
+
+            int elma = 0;
+            int armut = 0;
+            int muz = 0;
+
+            foreach (var key in kelimeler)
+            {
+                switch (key)
+                {
+                    case "elma":
+                        elma++;
+                        break;
+                    case "armut":
+                        armut++;
+                        break;
+
+                    case "muz":
+                        muz++;
+                        break;
+
+                }
+            }
+            Console.WriteLine("elma sayısı -> " + elma + "\n" + "armut sayısı -> " + armut + "\n" + "muz sayısı -> " + muz);
+        }
+    }
+}
